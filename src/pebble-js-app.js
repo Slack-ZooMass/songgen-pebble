7aefb126fb0fac260b83f700f7fb1fa99374e6b7
Ryan Stanley <stanleyrya@gmail.com>
2015-11-26 17:27:24 -0500

    JS now persists credentials, C messed up

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -9,15 +9,13 @@ Pebble.addEventListener('webviewclosed', function(e) {
   // Decode and parse config data as JSON
   var config_data = JSON.parse(decodeURIComponent(e.response));
   console.log('Config window returned: ', JSON.stringify(config_data));
-
-  // Prepare AppMessage payload
-  var dict = {
-    'KEY_ACCESS_TOKEN': config_data['access_token'],
-    'KEY_REFRESH_TOKEN': config_data['refresh_token']
-  };
+  
+  // Save to persistent storage on phone
+  localStorage.setItem('KEY_ACCESS_TOKEN', config_data['access_token']);
+  localStorage.setItem('KEY_REFRESH_TOKEN', config_data['refresh_token']);
 
   // Send settings to Pebble watchapp
-  sendToPebble(dict);
+  sendToPebble({ 'KEY_CREDENTIALS_SAVED': true });
 });
 
 // Watchapp
@@ -26,15 +24,28 @@ Pebble.addEventListener("ready", function(e) {
   console.log("PebbleKit JS ready!");
 
   // Notify the watchapp that it is now safe to send messages
-  Pebble.sendAppMessage({ 'KEY_JS_READY': true });
+  sendToPebble({ 'KEY_JS_READY': true });
 });
 
 Pebble.addEventListener('generate', function(e) {
   // Decode and parse data from Pebble watchapp
   var request = e.payload;
   console.log('Received message: ' + JSON.stringify(request));
-
-  generatePlaylist(request['KEY_ACCESS_TOKEN'], request['KEY_REFRESH_TOKEN'], request['KEY_WORDS']);
+  
+  var access_token = localStorage.getItem('KEY_ACCESS_TOKEN');
+  var refresh_token = localStorage.getItem('KEY_REFRESH_TOKEN');
+  var words = request['KEY_WORDS'];
+  
+  console.log('access_token: ' + access_token);
+  console.log('refresh_token: ' + refresh_token);
+  console.log('words: ' + words);
+  
+  if (access_token && refresh_token) {
+    generatePlaylist(access_token, refresh_token, words);
+  } else {
+    sendToPebble({ 'KEY_ERROR_CREDENTIALS_MISSING': true });
+  }
+  
 });
 
 function generatePlaylist(access_token, refresh_token, words) {
@@ -53,18 +64,17 @@ function generatePlaylist(access_token, refresh_token, words) {
         console.log(req.responseText);
         var response = JSON.parse(req.responseText);
         
-        // Prepare AppMessage payload
-        var dict = {};
-        if(response.refresh_token) {
-          dict['KEY_ACCESS_TOKEN'] = response.access_token;
-          dict['KEY_REFRESH_TOKEN'] = response.refresh_token;
+        if (response.refresh_token) {
+          // Save to persistent storage on phone
+          localStorage.setItem('KEY_ACCESS_TOKEN', config_data['access_token']);
+          localStorage.setItem('KEY_REFRESH_TOKEN', config_data['refresh_token']);
         }
-        dict['KEY_PLAYLIST_ID'] = response.playlistID;
         
         // Send response to Pebble watchapp
-        sendToPebble(dict);
+        sendToPebble({ 'KEY_PLAYLIST_ID': response.playlistID });
       } else {
-        console.log('Error');
+        console.log('Error with request');
+        sendToPebble({ 'KEY_ERROR_HTTP': true });
       }
     }
   };
@@ -73,7 +83,7 @@ function generatePlaylist(access_token, refresh_token, words) {
 
 function sendToPebble(dict) {
   console.log("Dictionary: " + JSON.stringify(dict));
-  Pebble.sendAppMessage(dict, function(){
+  Pebble.sendAppMessage(dict, function() {
     console.log('Sent result data to Pebble');
   }, function() {
     console.log('Failed to send result data!');
